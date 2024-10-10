076d497e8c69243f9f1eede633dccab8b5142ee3
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-10 10:33:39 +0400

    word-finder

---
+++ WordFinder/WordFinder/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WordFinder;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(WordFinder("the couch cat sat on the mat","cat"));
+    }
+
+    public static bool WordFinder(string myString, string myWord)
+    {
+        int i;
+        for (i = 0; i < myString.Length; i++)
+        {
+            int j;
+            for (j = 0; j < myWord.Length; j++)
+            {
+                if (myString[i+j] != myWord[j])
+                {
+                    break;
+                }
+            }
+            if (j == myWord.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
