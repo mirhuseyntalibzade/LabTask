6856f79c43ab4b6d12d19042d819dcc07ff2dc17
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-11-21 18:47:29 +0400

    pronia

---
+++ Pronia Project/Pronia Project/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia_Project.DAL.Contexts;
+using Pronia_Project.Models;
+using System;
+
+namespace Pronia_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProniaDBContext proniaDBContext = new();
+            //SliderItem item = new SliderItem()
+            //{
+            //    Title = "New Plant",
+            //    ShortDesc = "Pronia, With 100% Natural, Organic & Plant Shop.",
+            //    Offer = 65,
+            //    İmagePath = "1-1-524x617.png"
+            //};
+            //SliderItem item1 = new SliderItem()
+            //{
+            //    Title = "New Plant",
+            //    ShortDesc = "Pronia, With 100% Natural, Organic & Plant Shop.",
+            //    Offer = 65,
+            //    İmagePath = "1-1-524x617.png"
+            //};
+            //proniaDBContext.SliderItems.Add(item1);
+            //proniaDBContext.SaveChanges();
+
+            List<SliderItem> sliderItems = proniaDBContext.SliderItems.FromSql($"SELECT * FROM SliderItems").ToList();
+
+            return View(sliderItems);
+        }
+    }
+}
---
+++ Pronia Project/Pronia Project/Models/SliderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Pronia_Project.Models
+{
+    public class SliderItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDesc { get; set; }
+        public int Offer { get; set; }
+        public string İmagePath { get; set; }
+    }
+}
